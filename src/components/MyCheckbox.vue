5ccc2b74e2f0fd3f3a129f6a34f400d71c67a591
JunyeongYang <yang@junyeong.kr>
2019-09-13 18:48:51 +0900

    model decorator

---
+++ src/components/MyCheckbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="checkbox" :checked="checked" @change="change">
+</template>
+
+<script lang="ts">
+  import {Vue, Component, Model, Emit} from 'vue-property-decorator'
+
+  @Component
+  export default class MyCheckbox extends Vue {
+    @Model('change', {type: Boolean}) private readonly checked!: boolean
+
+    @Emit('changed')
+    private change(event: Event) {
+      return event.target.checked
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,8 @@
     <p>{{ count }}</p>
     <button @click="changeMessage">Change Message</button>
     {{count}}
+    <MyCheckbox v-model="checked" @changed="change" />
+    {{text}}
   </div>
 </template>
 
@@ -15,19 +17,23 @@ import { Component, Vue, Provide } from 'vue-property-decorator'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 import Message from '@/components/Message.vue'
 import Children from '@/components/Children.vue'
+import MyCheckbox from '@/components/MyCheckbox.vue'
 
 @Component({
   components: {
     HelloWorld,
     Message,
     Children,
+    MyCheckbox,
   },
 })
 export default class Home extends Vue {
   private message: string = 'hello world'
   private count: number = 0
+  private checked: boolean = false
+  private text: string = '동의하지 않습니다.'
 
-  @Provide('message') private msg: string = "Provide/Injdex example"
+  @Provide('message') private msg: string = 'Provide/Injdex example'
 
   private changeMessage() {
     this.message = 'changed Message'
@@ -35,6 +41,11 @@ export default class Home extends Vue {
 
   private counter() {
     this.count++
+  }
+
+  private change(checked: boolean) {
+    this.checked = checked
+    this.text = checked ? '동의합니다.' : '동의하지않습니다.'
   }
 
 
