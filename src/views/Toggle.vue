d40ac79eb7213755012027e8056909842e6aa795
JunyeongYang <yang@junyeong.kr>
2019-09-13 19:04:01 +0900

    mixins 초기 세팅

---
+++ src/views/Toggle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Dropdown />
+    <Accordion />
+  </div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator'
+  import Dropdown from '@/components/toggle/Dropdown.vue'
+  import Accordion from '@/components/toggle/Accordion.vue'
+
+  @Component({
+    components: {
+      Dropdown,
+      Accordion,
+    },
+  })
+  export default class Toggle extends Vue {
+
+  }
+</script>
+
+<style>
+
+</style>
